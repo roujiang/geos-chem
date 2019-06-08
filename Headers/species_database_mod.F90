24d908e4499eb6ec7eccbc48283267f649364e5a
Christoph A. Keller <cakelle2@discover07.prv.cube>
2019-06-08 14:14:43 -0400

    Use updated Henry coefficient for HCl in GEOS environment

--- Headers/species_database_mod.F90
+++ Headers/species_database_mod.F90
@@ -1610,8 +1610,10 @@ CONTAINS
              Is_Wetdep     = T
              DD_F0         = 0.0_fp
 #if defined( MODEL_GEOS )
-             DD_Hstar_old  = 2.05e+6_fp
-             Henry_K0      = 7.10e+15_f8
+             DD_Hstar_old  = 2.05e+13_fp
+             Henry_K0      = 7.00e+10_f8
+             !DD_Hstar_old  = 2.05e+6_fp
+             !Henry_K0      = 7.10e+15_f8
 #else
              DD_Hstar_old  = 2.05e+13_fp
              Henry_K0      = 7.00e+10_f8
